4cb011b1e55e4dee2e31c354528ac2aaba181265
seller123 <47704864+seller123@users.noreply.github.com>
2019-02-16 21:03:04 -0500

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,19 +1,31 @@
-/*
- * Discord Bot Builder Bot
- * Version 1.2.0
- * Robert Borghese
- */
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+import random
+from discord import Game
 
-const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
 
-if(!process.send) {
+Client = discord.client
+client = commands.Bot(command_prefix = '!')
+Clientdiscord = discord.Client()
 
-Files.initStandalone();
 
-} else {
+@client.event
+async def on_member_join(member):
+    print('Recognised that a member called ' + member.name + ' joined')
+    await client.send_message(member, 'Welcome to Sweatcoin Exchange/Buying/Selling + Services! Enjoy your time here and be sure to read the information and rules sections.')
+    print('Sent message to ' + member.name)
+async def on_ready():
+    await client.change_presence(game=Game(name='      '))
+    print('Ready, Freddy') 
 
-process.on('message', function(content) {
-	Files.initBotTest(content);
-});
 
-}
+@client.event
+async def on_message(message):
+    if message.content == 'swc!middleman':
+        await client.send_message(message.channel,'DM @Seller#0690 for a Middleman!')
+    if message.content.startswith('swc!bet'):
+        randomlist = ['win','lost']
+        await client.send_message(message.channel,(random.choice(randomlist)))
